bab20197d1485a57c18bb79e8d573b058558a1c6
ericstephen <EricWilliams@Erics-Macbook-Pro.local>
2014-07-29 09:34:32 -0400

    Tac Start

---
+++ Tac/Tac/TTTAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  TTTAppDelegate.h
+//  Tac
+//
+//  Created by Eric Williams on 7/29/14.
+//  Copyright (c) 2014 Eric Williams. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TTTAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
